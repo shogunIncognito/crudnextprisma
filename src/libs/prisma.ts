f674e4bdf739353b671792d0b1b79a20b8ed8ef6
BeiGod <saidsamuel2006@hotmail.com>
2023-09-03 07:29:32 -0500

    testing sqlite

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -6,6 +6,6 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const prisma = global.prisma ?? new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL } } })
+export const prisma = global.prisma ?? new PrismaClient({ datasources: { db: { url: 'file:./dev.db' } } })
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
