658298d7eb0475012d8628b6e2a43dadcc5f0dbc
BeiGod <saidsamuel2006@hotmail.com>
2023-09-03 07:59:25 -0500

    sql change

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -6,6 +6,6 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const prisma = global.prisma ?? new PrismaClient({ datasources: { db: { url: 'file:./dev.db' } } })
+export const prisma = global.prisma ?? new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL } } })
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
